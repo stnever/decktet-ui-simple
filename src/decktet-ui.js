ea12767d09fa8f43fea0c45d467b48b78ae13120
José Ventura <st.never@gmail.com>
2017-12-02 10:16:06 -0200

    refactor para usar npm

--- src/decktet-ui.js
+++ src/decktet-ui.js
@@ -1,6 +1,9 @@
+var _ = require('lodash'),
+    Decktet = require('decktet-utils')
+
 window.onload = function() {
 
-	var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '', 
+	var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '',
 		{ preload: preload, create: create });
 
 	// grupo que contem todas as cartas em jogo
@@ -23,25 +26,25 @@ window.onload = function() {
 		addUiButtons();
 
 		// copia a lista de todas as cartas e embaralha.
-		var deck = LangUtils.deepClone(Decktet.Cards);
-		DecktetUtils.shuffle(deck);
+		var deck = _.cloneDeep(Decktet.Cards);
+		Decktet.shuffle(deck);
 
 		// PhaserGroup que ira conter todas as cartas
 		allCards = game.add.group();
 
 		deck.forEach(function(card) {
 			// Cria uma sprite para cada carta.
-			var frameName = DecktetUtils.cardName(card).toLowerCase() + '.png';
+			var frameName = Decktet.cardName(card).toLowerCase() + '.png';
 			var cardSprite = allCards.create(64, 88, 'cards', frameName);
 			cardSprite.anchor.setTo(0.5, 0.5);
 			cardSprite.height = 176;
 			cardSprite.width = 128;
-			
+
 			// inicialmente em modo "moving"
 			cardSprite.inputEnabled = true;
 			cardSprite.input.enableDrag(false, true);
 			cardSprite.input.enableSnap(16, 16, true, true);
-			
+
 			// registra esta funcao para ser executada sempre que a carta for clicada
 			cardSprite.events.onInputDown.add(onCardClick);
 		});
@@ -51,19 +54,19 @@ window.onload = function() {
 		// centraliza o background
 		var bg = game.add.sprite(game.world.centerX, game.world.centerY, 'background');
 		bg.anchor.setTo(0.5, 0.5);
-		
+
 		// ajusta a escala para que cubra a tela inteira, mantendo o aspect ratio
 		var scaleX = game.width / bg.width;
 		var scaleY = game.height / bg.height;
 		var max = Math.max( scaleX, scaleY );
 		bg.scale.setTo( max, max );
 	}
-	
+
 	function addUiButtons() {
 		addButton( 'tap', onBtnTapClick );
 		addButton( 'move', onBtnMoveClick );
 		updateAllButtonsState(); // inicialmente 'move'
-		
+
 		// ajusta o layout e tamanho dos botoes, colocando-os no lado direito
 		// da pagina e um embaixo do outro.
 		var padding = 10;
@@ -82,14 +85,14 @@ window.onload = function() {
 		// ajusta a imagem de cada botao de acordo com o modo atual
 		buttons.forEach(function(btn) {
 			// over, out, down
-			var base = 'btn' + LangUtils.camelCase(btn.mode);
+			var base = _.camelCase('btn-' + btn.mode);
 			if ( mode == btn.mode )
 				btn.setFrames( base + 'Over.png', base + 'On.png', base + 'Off.png' );
 			else
 				btn.setFrames( base + 'Over.png', base + 'Off.png', base + 'On.png' );
 		});
 	}
-	
+
 	// adiciona um botao na lista. cada botao guarda o "modo" que ele ativa
 	// e a funcao de callback quando eh clicado.
 	function addButton( name, callback ) {
@@ -97,7 +100,7 @@ window.onload = function() {
 		btn.mode = name;
 		buttons.push(btn);
 	}
-	
+
 	// quando o botao 'tap' for clicado, ajusta o modo e desabilita o drag.
 	// Observe que o comportamento de virar esta na funcao 'onCardClick' que
 	// executa sempre, mas so faz efeito se mode=tap.
